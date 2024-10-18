ac048d60dd44ad19ee17a8a77349380fe6b7b4aa
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-10-19 01:29:23 +0530

    1

--- data_loaders/humanml/data/dataset.py
+++ data_loaders/humanml/data/dataset.py
@@ -279,12 +279,13 @@ class Text2MotionDatasetV2(data.Dataset):
         for name in tqdm(id_list):
             try:
                 # if True:
-                print(opt.motion_dir,name)
+                # print(opt.motion_dir,name)
                 motion = np.load(pjoin(opt.motion_dir, name + '.npy'))
                 if (len(motion)) < min_motion_len or (len(motion) >= 200):
                     continue
                 text_data = []
                 flag = False
+                print(opt.text_dir)
                 with cs.open(pjoin(opt.text_dir, name + '.txt')) as f:
                     for line in f.readlines():
                         text_dict = {}
@@ -335,7 +336,8 @@ class Text2MotionDatasetV2(data.Dataset):
                     new_name_list.append(name)
                     length_list.append(len(motion))
             except:
-                pass
+                print("HEelo")
+                print(name)
 
         name_list, length_list = zip(
             *sorted(zip(new_name_list, length_list), key=lambda x: x[1]))
