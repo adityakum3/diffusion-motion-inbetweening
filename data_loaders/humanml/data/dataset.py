ade7f22ffb375a8a3bffe35283eb64129cf6272f
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-10-19 13:35:47 +0530

    1

--- data_loaders/humanml/data/dataset.py
+++ data_loaders/humanml/data/dataset.py
@@ -279,14 +279,11 @@ class Text2MotionDatasetV2(data.Dataset):
         for name in tqdm(id_list):
             try:
                 # if True:
-                print(opt.motion_dir,name)
                 motion = np.load(pjoin(opt.motion_dir, name + '.npy'))
                 if (len(motion)) < min_motion_len or (len(motion) >= 200):
                     continue
-                print("hello")
                 text_data = []
                 flag = False
-                print(opt.text_dir)
                 with cs.open(pjoin(opt.text_dir, name + '.txt')) as f:
                     for line in f.readlines():
                         text_dict = {}
