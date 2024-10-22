bba597a61dd163d424bb6584cb0ba386fabeb332
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-10-22 23:58:12 +0530

    Changed readme

--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -11,16 +11,4 @@ mv ./t2/t2m/t2m .
 mv ./t2/t2m/kit .
 rm -rf ./t2
 
-gdown --fuzzy https://drive.google.com/file/d/1aP-z1JxSCTcUHhMqqdL2wbwQJUZWHT2j/view?usp=sharing
-unzip ./condmdi_random_joints.zip -d ./save/condmdi_random_joints
-rm ./condmdi_random_joints.zip
-
-gdown --fuzzy https://drive.google.com/file/d/15mYPp2U0VamWfu1SnwCukUUHczY9RPIP/view?usp=sharing
-unzip ./condmdi_random_frames.zip -d ./save/condmdi_randomframes
-rm ./condmdi_random_frames.zip
-
-gdown --fuzzy https://drive.google.com/file/d/1B0PYpmCXXwV0a5mhkgea_J2pOwhYy-k5/view?usp=sharing
-unzip ./condmdi_uncond.zip -d ./save/condmdi_uncond
-rm ./condmdi_uncond.zip
-
 echo -e "Downloading done!"
