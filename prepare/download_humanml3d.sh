15527b667aed8b757be9528cd02c39e0bb232fbd
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-11-02 02:22:36 +0530

    changes to prepare

--- prepare/download_humanml3d.sh
+++ prepare/download_humanml3d.sh
@@ -1,9 +1,9 @@
-!gdown https://drive.google.com/uc?id=1LlkZRMr18F4N_DpghIdyu8iZ5RxbjP3E
-!unzip ./HumanML3D.zip
-!mv ./HumanML3D ./dataset
-!rm ./HumanML3D.zip
-!unzip ./dataset/HumanML3D/texts.zip -d ./dataset/HumanML3D
-!rm ./dataset/HumanML3D/texts.zip
+gdown https://drive.google.com/uc?id=1LlkZRMr18F4N_DpghIdyu8iZ5RxbjP3E
+unzip ./HumanML3D.zip
+mv ./HumanML3D ./dataset
+rm ./HumanML3D.zip
+unzip ./dataset/HumanML3D/texts.zip -d ./dataset/HumanML3D
+rm ./dataset/HumanML3D/texts.zip
 gdown --fuzzy https://drive.google.com/file/d/1aP-z1JxSCTcUHhMqqdL2wbwQJUZWHT2j/view?usp=sharing
 unzip ./condmdi_random_joints.zip -d ./save/condmdi_random_joints
 rm ./condmdi_random_joints.zip
