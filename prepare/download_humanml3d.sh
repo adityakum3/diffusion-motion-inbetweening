7fb9da54b6edf7bac2a16bd83d3393dd935120b7
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-11-03 21:32:10 +0530

    Changed to run on kaggle

--- prepare/download_humanml3d.sh
+++ prepare/download_humanml3d.sh
@@ -15,14 +15,14 @@ rm ./condmdi_random_frames.zip
 gdown --fuzzy https://drive.google.com/file/d/1B0PYpmCXXwV0a5mhkgea_J2pOwhYy-k5/view?usp=sharing
 unzip ./condmdi_uncond.zip -d ./save/condmdi_uncond
 rm ./condmdi_uncond.zip
-!rm ./HumanML3D.zip
+rm ./HumanML3D.zip
 
 gdown --fuzzy https://drive.google.com/file/d/1BWlzD2kXZeqZpbzMfCS2qZIQa3CZpErc/view?usp=drive_link
-!unzip "./Archive (1).zip" 
-!rm -rf "./Archive (1).zip"
-!mv ./dataset/HumanML3D/new_joints/012314.npy ./new_joints
-!mv ./dataset/HumanML3D/new_joint_vecs/012314.npy ./new_joint_vecs
-!rm -rf ./dataset/HumanML3D/new_joints
-!rm -rf ./dataset/HumanML3D/new_joint_vecs
-!mv ./new_joints ./dataset/HumanML3D/new_joints
-!mv ./new_joint_vecs ./dataset/HumanML3D/new_joint_vecs
+unzip "./Archive (1).zip" 
+rm -rf "./Archive (1).zip"
+mv ./dataset/HumanML3D/new_joints/012314.npy ./new_joints
+mv ./dataset/HumanML3D/new_joint_vecs/012314.npy ./new_joint_vecs
+rm -rf ./dataset/HumanML3D/new_joints
+rm -rf ./dataset/HumanML3D/new_joint_vecs
+mv ./new_joints ./dataset/HumanML3D/new_joints
+mv ./new_joint_vecs ./dataset/HumanML3D/new_joint_vecs
--- utils/parser_util.py
+++ utils/parser_util.py
@@ -194,13 +194,13 @@ class TrainingOptions:
         metadata={
             "help": "If -1, will use all samples in the specified split."
         })
-    log_interval: int = field(default=10,
+    log_interval: int = field(default=1_000,
                               metadata={"help": "Log losses each N steps"})
     save_interval: int = field(
-        default=10,
+        default=10_000,
         metadata={"help": "Save checkpoints and run evaluation each N steps"})
     num_steps: int = field(
-        default=100,
+        default=1,
         metadata={
             "help": "Training will stop after the specified number of steps."
         })
