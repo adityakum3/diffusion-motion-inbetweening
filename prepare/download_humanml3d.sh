0d92c76a1dac025a757778f58a876b31e1172fdb
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-11-06 22:55:37 +0530

    Cleaning up

--- prepare/download_humanml3d.sh
+++ prepare/download_humanml3d.sh
@@ -4,14 +4,19 @@ mv ./HumanML3D ./dataset
 rm ./HumanML3D.zip
 unzip ./dataset/HumanML3D/texts.zip -d ./dataset/HumanML3D
 rm ./dataset/HumanML3D/texts.zip
+
+mkdir save
+mkdir save/condmdi_random_joints
 gdown --fuzzy https://drive.google.com/file/d/1aP-z1JxSCTcUHhMqqdL2wbwQJUZWHT2j/view?usp=sharing
 unzip ./condmdi_random_joints.zip -d ./save/condmdi_random_joints
 rm ./condmdi_random_joints.zip
 
+mkdir save/condmdi_randomframes
 gdown --fuzzy https://drive.google.com/file/d/15mYPp2U0VamWfu1SnwCukUUHczY9RPIP/view?usp=sharing
 unzip ./condmdi_random_frames.zip -d ./save/condmdi_randomframes
 rm ./condmdi_random_frames.zip
 
+mkdir save/condmdi_uncond
 gdown --fuzzy https://drive.google.com/file/d/1B0PYpmCXXwV0a5mhkgea_J2pOwhYy-k5/view?usp=sharing
 unzip ./condmdi_uncond.zip -d ./save/condmdi_uncond
 rm ./condmdi_uncond.zip
--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -2,8 +2,7 @@ echo -e "Downloading T2M evaluators"
 gdown --fuzzy https://drive.google.com/file/d/19gTDVtsdZNPjq-KCASSjrxAbIY4d99aD/view?usp=sharing
 rm -rf t2m
 
-unzip t2m.zip
-echo -e "Cleaning\n"
+
 tar -xvzf ./t2m.tar.gz 
 rm ./t2m.tar.gz
 mv ./t2m ./t2
