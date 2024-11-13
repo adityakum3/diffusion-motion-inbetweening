dae9e83ab0b959a9129024c5d63e89168834a1f5
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-11-13 15:26:11 +0530

    Changed the model to just generate

--- prepare/download_humanml3d.sh
+++ prepare/download_humanml3d.sh
@@ -5,8 +5,8 @@ rm ./HumanML3D.zip
 unzip ./dataset/HumanML3D/texts.zip -d ./dataset/HumanML3D
 rm ./dataset/HumanML3D/texts.zip
 
+mkdir save
 mkdir save/condmdi_randomframes
 gdown --fuzzy https://drive.google.com/file/d/15mYPp2U0VamWfu1SnwCukUUHczY9RPIP/view?usp=sharing
 unzip ./condmdi_random_frames.zip -d ./save/condmdi_randomframes
 rm ./condmdi_random_frames.zip
-
