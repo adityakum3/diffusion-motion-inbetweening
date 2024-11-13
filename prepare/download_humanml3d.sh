876a5870f0fd9a6c1a0f38247bc5f8a53f082ae0
adityakum3 <99905107+adityakum3@users.noreply.github.com>
2024-11-13 15:05:49 +0530

    Changed the model to just generate

--- prepare/download_humanml3d.sh
+++ prepare/download_humanml3d.sh
@@ -5,19 +5,8 @@ rm ./HumanML3D.zip
 unzip ./dataset/HumanML3D/texts.zip -d ./dataset/HumanML3D
 rm ./dataset/HumanML3D/texts.zip
 
-mkdir save
-mkdir save/condmdi_random_joints
-gdown --fuzzy https://drive.google.com/file/d/1aP-z1JxSCTcUHhMqqdL2wbwQJUZWHT2j/view?usp=sharing
-unzip ./condmdi_random_joints.zip -d ./save/condmdi_random_joints
-rm ./condmdi_random_joints.zip
+mkdir save/condmdi_randomframes
+gdown --fuzzy https://drive.google.com/file/d/15mYPp2U0VamWfu1SnwCukUUHczY9RPIP/view?usp=sharing
+unzip ./condmdi_random_frames.zip -d ./save/condmdi_randomframes
+rm ./condmdi_random_frames.zip
 
-
-gdown --fuzzy https://drive.google.com/file/d/1BWlzD2kXZeqZpbzMfCS2qZIQa3CZpErc/view?usp=drive_link
-unzip "./Archive (1).zip" 
-rm -rf "./Archive (1).zip"
-mv ./dataset/HumanML3D/new_joints/012314.npy ./new_joints
-mv ./dataset/HumanML3D/new_joint_vecs/012314.npy ./new_joint_vecs
-rm -rf ./dataset/HumanML3D/new_joints
-rm -rf ./dataset/HumanML3D/new_joint_vecs
-mv ./new_joints ./dataset/HumanML3D/new_joints
-mv ./new_joint_vecs ./dataset/HumanML3D/new_joint_vecs
